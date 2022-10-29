40a5f6119a9859aff87c238d13aeffe513b1790e
Mohammad Javid <mjavid@cma.com>
2022-10-29 20:45:29 +0530

    dom manip

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ class MainModule {
   public static entryPoint() {
     console.log(DomApi.Name);
     DomApi.func();
+    console.log(document.getElementsByTagName("h1")[0]);
   }
 }
 MainModule.entryPoint();
